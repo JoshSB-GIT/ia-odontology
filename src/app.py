b33a5a02bc59048744a16a5a7c6a95620e35bde6
JoshSB-GIT <sierrajoseph8013@gmail.com>
2023-04-08 08:33:32 -0500

    cambios en app

--- src/app.py
+++ src/app.py
@@ -3,16 +3,18 @@ from routes.usersRoutes import users
 from routes.authRoutes import auth
 from routes.chatBotRoutes import chatbot
 from flask_mysqldb import MySQL
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 MySQL(app)
+CORS(app)
 
 
 app.register_blueprint(users)
 app.register_blueprint(auth)
 app.register_blueprint(chatbot)
 
-
+@cross_origin
 @app.route('/')
 def home():
     return jsonify({'message': '¡Welcome!'})
