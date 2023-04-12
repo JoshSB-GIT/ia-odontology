224d92a1a43ff2acbe0ac34c605e9b2e7f7fa398
JoshSB-GIT <sierrajoseph8023@gmail.com>
2023-04-12 12:24:49 -0500

    final front-end

--- src/app/components/chabot/chabot.component.ts
+++ src/app/components/chabot/chabot.component.ts
@@ -1,28 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ChatserviceService } from 'src/app/services/chatservice.service';
 
 @Component({
   selector: 'app-chabot',
   templateUrl: './chabot.component.html',
-  styleUrls: ['./chabot.component.css']
+  styleUrls: ['./chabot.component.css'],
 })
 export class ChabotComponent {
-
+  @ViewChild('myDiv', { static: false }) myDivRef: ElementRef | null = null;
   input_user: string = '';
   answers: any = [];
 
-  constructor(private chatService: ChatserviceService) {
-
-  }
+  constructor(private chatService: ChatserviceService) {}
 
   send() {
     if (this.input_user != '') {
-      const data = { input_user: this.input_user }
+      const data = { input_user: this.input_user };
       this.chatService.sendQuestion(data).subscribe((res: any) => {
         this.answers.push(res);
-        this.input_user = ''
+        this.input_user = '';
       });
     }
   }
 
+  scroll_bottom() {
+    if (this.myDivRef) {
+      this.myDivRef.nativeElement.scrollTop =
+        this.myDivRef.nativeElement.scrollHeight;
+    }
+  }
+
+  execute_functs(): void {
+    this.send();
+    setTimeout(() => {
+      this.scroll_bottom();
+    }, 100);
+  }
 }
